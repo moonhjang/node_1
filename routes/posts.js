e41b8ee82cc95872cab9ebf98038d43848e922d6
Moonhee <o119a@naver.com>
2022-03-21 10:32:51 +0900

    20220321_available to delete posts in DB

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Posts = require("../schemas/posts")
+const List = require("../schemas/list")
 const router = express.Router();
 
 // const bodyParser = require("body-parser");  //post ejs 양식을 위해 추가됨
@@ -35,20 +36,49 @@ router.get('/posts/:postsId', async (req, res) => {
 
 });
 
-// Posting 등록 (DB에 내용 넣기)
-router.post("/posts", async (req, res) => {
-    const { postsId, password, user, title, content, createdAt } = req.body;
 
-    const posts = await Posts.find({ postsId });
+// posting을 리스트에 추가 (삭제예정)
+router.post("/posts/:postsId/list", async (req, res) => {
+    const { postsId } = req.params;
+    const { quantity } = req.body;
+  
+    const existsLists = await List.find({ postsId: Number(postsId) });
+    if (existsLists.length) {
+      return res.json({ success: false, errorMessage: "이미 리스트에 존재하는 포스트입니다." });
+    }
+  
+    await List.create({ postsId: Number(postsId), quantity: quantity });
+    res.json({ result: "success" });
+}); 
+
+
+
+// Posting을 리스트에서 삭제 
+router.delete("/posts/:postsId/list", async (req, res) => {
+    const { postsId } = req.params;
+
+    const existsLists = await List.find({ postsId: Number(postsId) }); 
+    if (existsLists.length > 0) {
+        await List.deleteOne({ postsId });
+      }
+
+    res.json({ result: "success" });
+});
+
+ 
+// posting 등록 (DB에 내용 넣기)
+router.post("/posts", async (req,res) => {
+    const { postsId, user, password, title, content, createdAt } = req.body;
+
+    const posts = await Posts.find({postsId});
     if (posts.length) {
-        return res
-        .status(400)
-        .json({ success: false, errorMessage: "이미 있는 데이터입니다." });
+      return res.status(400).json({ success: false, errorMessage: "이미 있는 데이터입니다."});
     }
 
-    const createdPosts = await Posts.create({ postsId, password, user, title, content, createdAt })
+    const createdPosts = await Posts.create({ postsId, user, password, title, content, createdAt  })
 
-    res.json({ posts: createdPosts });
+    res.json({ posts: createdPosts});
 });
+
 
 module.exports = router;
